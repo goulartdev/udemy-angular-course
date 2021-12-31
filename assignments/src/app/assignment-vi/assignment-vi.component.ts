e90273f7b695b386266bea9e284de40fa72e828b
Djonathan Goulart <d.goulart@outlook.com.br>
2021-12-30 23:07:53 -0300

    assignment 6: display data after submitting

--- assignments/src/app/assignment-vi/assignment-vi.component.ts
+++ assignments/src/app/assignment-vi/assignment-vi.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+interface Subscription {
+  email: string,
+  type: string,
+  password: string
+}
+
 @Component({
   selector: 'app-assignment-vi',
   templateUrl: './assignment-vi.component.html',
@@ -8,6 +14,7 @@ import { NgForm } from '@angular/forms';
 })
 export class AssignmentVIComponent implements OnInit {
   subscriptions: string[] = ["basic", "advanced", "pro"];
+  subscription: Subscription | null = null;
 
   constructor() { }
 
@@ -19,7 +26,8 @@ export class AssignmentVIComponent implements OnInit {
   }
 
   onSubmit(form: NgForm) {
-    console.log(form)
+    console.log(form);
+    this.subscription = form.value;
   }
 
 }
